e230642e51cf8a1c28a4b517af7ac0ed0f13e2fe
Tommy Ekh <tommy.ekh@coop.se>
2019-07-07 09:58:42 +0200

    Reading MQTT

--- Services/MQTTService.cs
+++ Services/MQTTService.cs
@@ -8,6 +8,7 @@ using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Options;
 using MQTTnet.Diagnostics;
+using MQTTnet.Protocol;
 
 namespace powerconcern.mqtt.services
 {
@@ -41,16 +42,25 @@ namespace powerconcern.mqtt.services
             Logger.LogTrace(e.TraceMessage.Message);
         }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             Logger.LogInformation("Background thread started");
             
+            
+
+            var options = new MqttClientOptionsBuilder()
+            .WithClientId(Guid.NewGuid().ToString())
+            .WithTcpServer("mqtt.symlink.se")
+            .WithCredentials("fredrik:fredrik", "aivUGL6no")
+            .WithCleanSession()
+            .Build();
+            /*
             var options = new MqttClientOptionsBuilder()
             .WithClientId(Guid.NewGuid().ToString())
             .WithTcpServer("192.168.222.20", 1883)
             .WithCleanSession()
             .Build();
-
+*/
             stoppingToken.Register(() => Console.WriteLine("Background svc is stopping."));
 
             // Connecting
@@ -62,28 +72,63 @@ namespace powerconcern.mqtt.services
                 Console.WriteLine("### CONNECTED WITH SERVER ###");
 
                 // Subscribe to a topic
-                await client.SubscribeAsync(new TopicFilterBuilder().WithTopic("mytopic").Build());
+                await client.SubscribeAsync(new TopicFilterBuilder().WithTopic("CurrentMeter/#").Build());
+                await client.SubscribeAsync(new TopicFilterBuilder().WithTopic("EVCharger/#").Build());
 
                 Console.WriteLine("### SUBSCRIBED ###");
             });
 
-            client.UseApplicationMessageReceivedHandler(e =>
-            {
-                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
-                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
-                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
-                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
-                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
-                Console.WriteLine();
+            stoppingToken.Register(() =>
+            Logger.LogDebug($" MQTTSvc background task is stopping."));
 
-                if(client != null) {
-                    Task.Run(() => client.PublishAsync("hello/world"));
-                }
-            });
+
+/* Python rules
+    client.subscribe("EVCharger/#")
+    client.subscribe("CurrentMeter/#")
+
+
+    if msg.topic == "EVCharger/status/current":
+        charge_current = float(msg.payload)
+        print("Charge current %.1f A" % charge_current)
+    if msg.topic == "CurrentMeter/status/current":
+        current = float(msg.payload)
+        mean_current = (9*mean_current + current) / 10
+        print("Current %.1f (mean %.1f)" % (current, mean_current))
+        if mean_current > max_current:
+            new_charge_current = charge_current - (mean_current-max_current)
+            client.publish("EVCharger/set/current", payload=str(int(new_charge_current)), qos=0, retain=False)
+            if new_charge_current < 2:
+                client.publish("EVCharger/set/enable", payload=str(0), qos=0, retain=False)
+        else:
+            client.publish("EVCharger/set/current", payload=str(int(max_current)), qos=0, retain=False)
+            client.publish("EVCharger/set/enable", payload=str(1), qos=0, retain=False)
+ */
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(250, stoppingToken);
+                //Logger.LogDebug("Waiting for messages");
+                client.UseApplicationMessageReceivedHandler(e =>
+                {
+                    Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                    Console.WriteLine();
+
+                    if(client != null) {
+                        /*Task.Run(() => client.PublishAsync("hello/world",
+                                                            "4",
+                                                            MqttQualityOfServiceLevel.AtLeastOnce,
+                                                            true));
+                        */
+                    }
+                });
+            }
 
             Console.WriteLine("Background svc is stopping.");
             
-            return Task.CompletedTask;
+            //return Task.CompletedTask;
         }
     }
 }
